19f2e288a96973d226ffa3108d625b5a09f05d5a
Hamed Zandieh <hamed.zandieh@gmail.com>
2024-06-20 18:38:08 +0330

    Update HZ_2DEyesNetwork.py

--- HZ_2DEyesNetwork.py
+++ HZ_2DEyesNetwork.py
@@ -234,12 +234,8 @@ for side in "RL":
             cmds.parent( sec_tex, eye_ctl )
 
 
-pp_geo = cmds.polyPlane(cuv=2, sy=10, sx=10, h=1, ch=1, w=1, ax=(0, 1, 0))
-cmds.setAttr("%s.rotateX"%pp_geo[0], 90)
-cmds.setAttr("%s.subdivisionsWidth"%pp_geo[1], 3)
-cmds.setAttr("%s.subdivisionsHeight"%pp_geo[1], 3)
-cmds.setAttr("%s.width"%pp_geo[1], 8)
-cmds.setAttr("%s.height"%pp_geo[1], 8)
+pp_geo,  = cmds.polyPlane(cuv=2, sy=3, sx=3, h=8, ch=1, w=8, ax=(0, 1, 0))
+cmds.setAttr("%s.rotateX"%pp_geo, 90)
 cmds.sets(forceElement='eyesLayeredShader12SG', e=1)
 cmds.move( -1.5, 0, 0,'eye_R_ctl', r=1, os=1, wd=1)
 cmds.move( 1.5, 0, 0, 'eye_L_ctl', r=1, os=1, wd=1)
